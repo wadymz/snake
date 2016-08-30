60071cf5f7be0784ef30e2894bb9b02cea689100
wadymz <zobin.vadym@gmail.com>
2016-08-30 19:38:15 +0200

    создали класс Точка

---
+++ snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        public int x, y;
+        public char sym;
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
